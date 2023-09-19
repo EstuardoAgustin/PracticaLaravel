5a81957493d52fe355a396e70aa6b67b8c423e10
stuar4 <eagustinm3@miumg.edu.gt>
2023-09-18 23:10:24 -0600

    feat: Opcion Eliminar
    Se agrega la funcion Eliminar
    
    Se crea un modal para editar y se crea un modal para eliminar

--- resources/views/categorias/index.blade.php
+++ resources/views/categorias/index.blade.php
@@ -36,9 +36,12 @@
                         <div class="col-md-3 d-flex justify-content-end">
                           
                              
-                 
                              <!-- Button trigger modal -->
-                            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#{{$categoria->id}}">
+                             <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#editar-{{$categoria->id}}">
+                                Editar
+                            </button>
+                             <!-- Button trigger modal -->
+                            <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#{{$categoria->id}}">
                                 Eliminar
                             </button>
                         </div>
@@ -46,7 +49,7 @@
 
                    
   
-                 <!-- Modal -->
+                <!-- Modal ELIMINAR-->
                     <div class="modal fade" id="{{$categoria->id}}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
                         <div class="modal-dialog">
                         <div class="modal-content">
@@ -55,7 +58,7 @@
                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                             </div>
                             <div class="modal-body">
-                            Esta seguro que desea esta categoria: 
+                            Esta seguro que desea Eliminar esta categoria: 
                             {{$categoria->name}}
                             </div>
                             <div class="modal-footer">
@@ -71,6 +74,51 @@
                         </div>
                         </div>
                     </div>
+                <!-- Fin Modal -->
+
+                <!-- Modal EDITAR-->
+                <div class="modal fade" id="editar-{{$categoria->id}}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="exampleModalLabel">Editar Categoria</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                        Desea Editar esta categoria: 
+                        {{$categoria->name}}
+                        </div>
+                        <form action="{{ route('categorias.update' , ['categoria'=> $categoria -> id])}}" method="POST">
+                            @csrf
+                            @method('PATCH')
+                            @if (session('success'))
+                                <h6 class="alert alert-success">{{session('success')}}</h6>
+
+                            @endif
+                            @error('title')
+                            <h6 class="alert alert-danger">{{$message}}</h6>    
+
+                            @enderror
+
+                            <div class="mb-3">
+                                <label for="name" class="form-label">Titulo de la categoria</label>
+                                <input type="text" class="form-control" name="name" value="{{$categoria->name}}" >
+                            </div>
+                            <div class="mb-3">
+                                <label for="color" class="form-label">Color de la categoria</label>
+                                <input type="color" class="form-control" name="color" value="{{$categoria->color}}">
+                            </div>
+
+                            <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="submit" class="btn btn-primary">Actualizar</button>
+                        </form>
+                        
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            <!-- Fin Modal -->
                 @endforeach
               </div>
         </div>
